91a124c18e415cffb0ee859d50a4374a3b49efe7
BrxwnSugxr <jsdev.mas@gmail.com>
2025-02-09 16:47:16 +1100

    Logic constants

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,10 @@
 // create an array of choices
-
+export const CHOICES = ["R", "P", "S"];
+export const EMOJI = {
+  R: "✊ Rock",
+  P: "✋ Paper",
+  S: "✌ Scissors",
+};
 // create emoji objective
 
 // export max history
