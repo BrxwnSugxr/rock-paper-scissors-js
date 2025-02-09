d5a2818a1bef80d52a6857217edf17c79742bf22
BrxwnSugxr <jsdev.mas@gmail.com>
2025-02-09 16:59:21 +1100

    game logic

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,5 +1,16 @@
 // import constant and two variables
-
+import { CHOICES, WIN_CONDITIONS } from "./constants.js";
 // export computer function
-
+export function getComputerChoice() {
+  return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
 // export determine choice function
+export function determineResult(userChoice, getComputerChoice) {
+  if (userChoice === getComputerChoice) {
+    return "tie";
+  } else if (WIN_CONDITIONS[userChoice] === getComputerChoice) {
+    return "win";
+  } else {
+    return "lose";
+  }
+}
