dc70c344dd202ba33260538e07456ae919629bb5
BrxwnSugxr <jsdev.mas@gmail.com>
2025-02-09 16:51:55 +1100

    logic of contants

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,12 +1,19 @@
-// create an array of choices
+// Game choices
 export const CHOICES = ["R", "P", "S"];
+
+// Emoji for each Choice
 export const EMOJI = {
   R: "✊ Rock",
   P: "✋ Paper",
   S: "✌ Scissors",
 };
-// create emoji objective
 
-// export max history
+// max number of matches to show in history
+export const MAX_HISTORY = 5;
 
-// export win condition
+// win condition (what beats what)
+export const WIN_CONDITIONS = {
+  R: "S", //Rock beats Scissors
+  P: "R", //Paper beats Rock
+  S: "P", // Scissors beat Paper
+};
