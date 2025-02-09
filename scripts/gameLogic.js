8b80c354436ed1791c0245416327e46597d6ed0a
BrxwnSugxr <jsdev.mas@gmail.com>
2025-02-09 17:01:18 +1100

    fixed up commenting

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,6 +1,14 @@
+/**
+ * getComputerChoice(): Randomly selects Rock (R), Paper (P), or Scissors (S) for the computer.
+ * determineResult(): Compares the user's choice with the computer's choice and returns:
+ * "tie" if both choices are the same.
+ * "win" if the user's choice beats the computer's choice.
+ * "lose" if the computer's choice beats the user's choice.
+ */
+
 // import constant and two variables
 import { CHOICES, WIN_CONDITIONS } from "./constants.js";
-// export computer function
+// Get the computer's choice (randomly select R, P, or S)
 export function getComputerChoice() {
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
